c9dbdd603e1778c615e30b19882443eba66666ab
SYOMAS-PC1\Owner <syoma110514@gmail.com>
2025-12-23 18:19:00 +0900

    Did homework

---
+++ Assets/Script/Image use.cs
@@ -0,0 +1,19 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+
+public class Imageuse : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Image>().fillAmount += 0.01f;
+    }
+}
