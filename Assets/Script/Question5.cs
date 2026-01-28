f328d575663fe43d364c4f96fbe2e12717afaaf2
SYOMAS-PC2\syoma <syoma110514@gmail.com>
2026-01-28 21:41:46 +0900

    add Question 5

---
+++ Assets/Script/Question5.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Question5 : MonoBehaviour
+{
+    AudioSource a;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Onclicked()
+    {
+        a.Play();
+
+    }
+}
