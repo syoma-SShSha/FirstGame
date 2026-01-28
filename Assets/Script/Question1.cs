3a6b189e43c1cfff9d649cfa4b8292f6c18eabf1
SYOMAS-PC2\syoma <syoma110514@gmail.com>
2026-01-28 21:00:38 +0900

    complete question 3

--- Assets/Script/Question1.cs
+++ Assets/Script/Question1.cs
@@ -3,17 +3,29 @@ using UnityEngine;
 public class Question1 : MonoBehaviour
 {
     public GameObject Cube;
+    private GameObject instance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i = 0;i <= 8; i++)
+        for (int i = 0; i <= 8; i++)
         {
-            for (int a = 0;a <= 8; a++)
+            for (int a = 0; a <= 8; a++)
             {
-                Instantiate(Cube, new Vector3(a * 2, 0, i * 2), Quaternion.identity);
+                instance = Instantiate(Cube, new Vector3(a * 2, 0, i * 2), Quaternion.identity);
+                if ((instance.transform.position.x) >= 10 && (instance.transform.position.z) <= 10)
+                {
+                    instance.GetComponent<MeshRenderer>().material.color = Color.blue;
+                }
+                if ((instance.transform.position.x) <= 10 && (instance.transform.position.z) >= 10)
+                {
+                    instance.GetComponent<MeshRenderer>().material.color = Color.green;
+                }
+                if ((instance.transform.position.x) >= 10 && (instance.transform.position.z) >= 10)
+                {
+                    instance.GetComponent<MeshRenderer>().material.color = Color.skyBlue;
+                }
             }
         }
-        
     }
 
     // Update is called once per frame
