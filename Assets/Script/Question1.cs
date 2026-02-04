cbc23891ed44120ff4cb9e1a4b59064700077a62
SYOMAS-PC2\syoma <syoma110514@gmail.com>
2026-02-04 21:05:34 +0900

    complete question 5

--- Assets/Script/Question1.cs
+++ Assets/Script/Question1.cs
@@ -4,14 +4,19 @@ public class Question1 : MonoBehaviour
 {
     public GameObject Cube;
     private GameObject instance;
+    private Rigidbody[] storage;
+    private int count = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        storage = new Rigidbody[81];
         for (int i = 0; i <= 8; i++)
         {
             for (int a = 0; a <= 8; a++)
             {
                 instance = Instantiate(Cube, new Vector3(a * 2, 0, i * 2), Quaternion.identity);
+                storage[count] = instance.GetComponent<Rigidbody>();
+                count++;
                 if ((instance.transform.position.x) >= 10 && (instance.transform.position.z) <= 10)
                 {
                     instance.GetComponent<MeshRenderer>().material.color = Color.blue;
@@ -32,5 +37,12 @@ public class Question1 : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Gravity()
+    {
+        for(count = 0; count < 81; count++)
+        {
+            storage[count].useGravity = true;
+        }
     }
 }
