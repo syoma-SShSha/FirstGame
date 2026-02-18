6e2a5ec19b8d7423a34486629f9585523686e056
SYOMAS-PC2\syoma <syoma110514@gmail.com>
2026-02-18 21:37:43 +0900

    Add spawner class

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+public class Spawner : MonoBehaviour
+{
+    public int waveCount = 1;
+    public List<GameObject> wave1 = new List<GameObject>();
+    public List<GameObject> wave2 = new List<GameObject>();
+    public List<GameObject> wave3 = new List<GameObject>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        AllInstantiate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AllInstantiate()
+    {
+        switch(waveCount)
+        {
+            case 1:
+                for(int i = 0;i < wave1.Count; i++)
+                {
+                    Instantiate(wave1[i], new Vector3(i * 2, 0, 0), Quaternion.identity);
+                }
+                break;
+            case 2:
+                for (int i = 0; i < wave2.Count; i++)
+                {
+                    Instantiate(wave2[i], new Vector3(i * 2, 0, 0), Quaternion.identity);
+                }
+                break;
+            case 3:
+                for (int i = 0; i < wave3.Count; i++)
+                {
+                    Instantiate(wave3[i], new Vector3(i * 2, 0, 0), Quaternion.identity);
+                }
+                break;
+        }
+        
+    }
+}
