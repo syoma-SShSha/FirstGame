fc2200b6f3e69d9ef89402dac606d9c9f0c83cba
SYOMAS-PC2\syoma <syoma110514@gmail.com>
2026-04-03 16:11:08 +0900

    add Enemy3 and Bullet2

---
+++ Assets/Script/EnemyBullet.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float time;
+    public GameObject bullet;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time + Time.deltaTime;
+        if (time >= 3f)
+        {
+            time = 0;
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+        
+    }
+}
